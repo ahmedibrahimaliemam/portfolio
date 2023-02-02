b1947b04e8a4658a852d38289fc8d78ce5abecf7
ahmedibrahimaliemam <ahmedibrahimaliemam@gmail.com>
2023-02-02 02:26:58 +0200

    change navebar color

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 let services=document.getElementById("services") ;
 let projects=document.getElementById("projects") ;
 let contact=document.getElementById("contact") ;
-let logo=document.getElementsByClassName("name") ;
 //declear a variables of projects item
 let item_zero=document.getElementById("item-zero") ;
 let item_one=document.getElementById("item-one") ;
@@ -10,7 +9,12 @@ let item_two=document.getElementById("item-two") ;
 let item_three=document.getElementById("item-three") ;
 let item_four=document.getElementById("item-four") ;
 let item_five=document.getElementById("item-five") ;
+let ahmed=document.querySelector(".name") ;
 //events of projects
+ahmed.onclick=()=>{
+
+}
+
 item_zero.onclick=function(){
     window.location="https://ahmedibrahimaliemam.github.io/E-commerce/" ;
 }
@@ -24,34 +28,27 @@ item_three.onclick=function(){
     window.location="https://ahmedibrahimaliemam.github.io/templatehtml-css/" ;
 }
 item_four.onclick=()=>{
-    window.location="https://react-e-commerce-app5.netlify.app" ;
+    window.location="https://react-e-commerce.surge.sh/" ;
 }
 //end events of projects
-logo.onclick=function(){
-    window.scrollTo({
-        top: 0 , 
-        left: 0 , 
-        behavior: 'smooth'
-    }) ;
-}
 console.log(services,projects,contact) ;
 services.onclick=function(){
     window.scrollTo({
-        top: 600,
+        top: 640,
         left: 0,
         behavior: 'smooth'
       });
 }
 projects.onclick=function(){
     window.scrollTo({
-        top: 1100,
+        top: 1351,
         left: 0,
         behavior: 'smooth'
       });
 }
 contact.onclick=function(){
     window.scrollTo({
-        top: 1900,
+        top: 2200,
         left: 0,
         behavior: 'smooth'
       });
@@ -70,14 +67,14 @@ function init(){
         }
         projects.onclick=function(){
             window.scrollTo({
-                top: 1850,
+                top: 2250,
                 left: 0,
                 behavior: 'smooth'
               });
         }
         contact.onclick=function(){
             window.scrollTo({
-                top: 4070,
+                top: 4500,
                 left: 0,
                 behavior: 'smooth'
               });
